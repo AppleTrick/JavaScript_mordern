6d8e1ba0e3a621147db437cef06392a2f6f0d29c
Park <arrans9591@gmail.com>
2021-03-03 21:26:53 +0900

    tictactoc

--- javascript_zerocho/tictactoc/tictactoc.js
+++ javascript_zerocho/tictactoc/tictactoc.js
@@ -1,10 +1,21 @@
 var body = document.body;
 var table = document.createElement('table');
+var lineValue = [];
+var boxValue = []; 
+
+var callsB = function(e){
+   
+};
 
 for(i=1; i<=3; i++){
     var line = document.createElement('tr');
+    lineValue.push(line);
+    boxValue.push([]);
     for(j=1; j<=3; j++){
         var box = document.createElement("td");
+
+        box.addEventListener('click',callsB)
+        boxValue[i-0].push(box);
         line.appendChild(box);
     }
     table.appendChild(line);
