b6050bbc89e1352c25cf4425cd1950a544c15799
Changhee Park <arrans9591@gmail.com>
2021-05-07 17:49:31 +0900

    javascirpt learning

---
+++ javascript_book/027Array/add.js
@@ -0,0 +1,6 @@
+const arr = [];
+arr[0] = 1;
+arr['ba'] = 'ba';
+
+console.log(arr);
+console.log(arr[0]);
---
+++ javascript_book/027Array/crate.js
@@ -0,0 +1,14 @@
+const arr = new Array(1);
+console.log(arr);
+//[ <1 empty item> ]
+
+const arr1 = new Array(1,2,3);
+console.log(arr1);
+//[ 1, 2, 3 ]
+
+const arr2 = Array.of(1);
+console.log(arr2);
+//[ 1 ]
+
+const arr3 = Array.from({0:'a',1:'b',length:2})
+console.log(arr3);
