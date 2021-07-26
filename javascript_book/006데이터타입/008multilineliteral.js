83a6c9510fbe8bd64f9c37ad1906d3127424a6e8
Changhee Park <arrans9591@gmail.com>
2021-07-26 19:29:13 +0900

    6장 공부내용

---
+++ javascript_book/006데이터타입/008multilineliteral.js
@@ -0,0 +1,9 @@
+var templete = '<ul>\n\t<li><a href="#">Home</a></li>\n</ul>'
+
+var templete1 = `<ul>
+    <li><a href="#">Home</a></li>
+</ul>`
+
+console.log(templete);
+
+console.log(templete1);
---
+++ javascript_book/006데이터타입/009expressionLiteral.js
@@ -0,0 +1,10 @@
+var first = 'LostArk';
+var second = 'best';
+
+console.log( `${first} is ${second} game`);
+
+console.log( first + ' is ' + second + ' game');
+
+console.log( `${first} is ${second} game`);
+
+console.log( `1 + 2 = ${1+2} `);
