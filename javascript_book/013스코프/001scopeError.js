bb65246d41bd4b1066ce5ba73962ba199c1c4724
AppleTrick <arrans9591@gmail.com>
2021-08-03 21:22:45 +0900

    scope 부분 내용 수정

--- javascript_book/013스코프/001scopeError.js
+++ javascript_book/013스코프/001scopeError.js
@@ -4,3 +4,6 @@ function add(x,y){
 }
 
 console.log(add(2,5));
+
+// 매개변수는 함수 몸체 내부에서만 참조가능
+//console.log(x,y);
--- javascript_book/013스코프/002scopeUseError.js
+++ javascript_book/013스코프/002scopeUseError.js
@@ -1,14 +1,14 @@
-var var1 = 1;
+var var1 = 1;           // 코드의 가장 바깥 영역에서 선언한 변수
 
 if (true) {
-    var var2= 2;
+    var var2= 2;        // 코드 블록 내에서 선언한 변수
     if (true) {
-        var var3 =3;
+        var var3 =3;    // 중첨된 코드 블록 내에서 선언된 변수
     }
 }
 
 function foo(){
-    var var4 = 4;
+    var var4 = 4;   // 함수 내에서 선언된 변수
 
     function bar(){
         var var5 = 5; // 중첩된 함수
