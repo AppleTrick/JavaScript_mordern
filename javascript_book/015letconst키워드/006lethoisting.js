0b1c0b349c22e6b1f254613f31a68cd1fba6c863
AppleTrick <arrans9591@gmail.com>
2021-08-08 09:39:31 +0900

    const 공부

---
+++ javascript_book/015letconst키워드/006lethoisting.js
@@ -0,0 +1,6 @@
+let data1 = 1;    // 전역 변수
+console.log(data1);
+{
+    console.log(data1);
+    let data1 = 2;
+}
---
+++ javascript_book/015letconst키워드/008constHoisting.js
@@ -0,0 +1,9 @@
+{
+    // console.log(a);
+    // 호이스팅이 발생하지 않는 것 처럼 동작
+    const a = 1;
+
+    console.log(a);
+}
+// 블록레벨 스코프를 가진다.
+//console.log(a);
